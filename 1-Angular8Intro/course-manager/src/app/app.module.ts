c58c079b26dd9353fde3640ef357b65bf786dd72
eduardo <eduardormonteiro@gmail.com>
2021-02-16 22:08:59 -0300

    fist commit part1

---
+++ 1-Angular8Intro/course-manager/src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { CourseModule } from './courses/course.module';
+import { CoreModule } from './core/core.module';
+import { Error404Component } from './core/component/error-404/error-404.component';
+import { CourseListComponent } from './courses/course-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Error404Component
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      {
+        path: '', redirectTo: 'courses', pathMatch: 'full'
+      },
+      {
+        path: 'courses', component: CourseListComponent
+      },
+      {
+        path: 'courses/info/:id', component: CourseListComponent
+      },
+      {
+        path: '**', component: Error404Component
+      }
+    ]),
+    CourseModule,
+    CoreModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
