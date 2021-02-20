a4c2f490ae01b514559454d62e6f90f3a11f6467
eduardo <eduardormonteiro@gmail.com>
2021-02-20 14:22:40 -0300

    add pages start

---
+++ 7-weather-api/api-weather/src/app/pages/bookmarks/bookmarks.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'erm-bookmarks',
+  templateUrl: './bookmarks.page.html',
+  styleUrls: ['./bookmarks.page.css']
+})
+export class BookmarksPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
