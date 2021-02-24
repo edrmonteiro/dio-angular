06799c8f3060f9be1fbeaad83bd07ec99ffe3bc1
eduardo <eduardormonteiro@gmail.com>
2021-02-23 22:02:56 -0300

    project basis

--- 4-spa-development/spaangular/src/app/app.module.ts
+++ 4-spa-development/spaangular/src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { ListActivitiesComponent } from './list-activities/list-activities.component';
+import { AddActivitiesComponent } from './add-activities/add-activities.component';
+import { RankComponent } from './rank/rank.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ListActivitiesComponent,
+    AddActivitiesComponent,
+    RankComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
