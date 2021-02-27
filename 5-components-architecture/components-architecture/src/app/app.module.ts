88ce8d7bbd82d1526320c831170d097b075c0ada
eduardo <eduardormonteiro@gmail.com>
2021-02-27 09:24:18 -0300

    Components- Archtecture revhitecture

---
+++ 5-components-architecture/components-architecture/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DataBindingComponent } from './data-binding/data-binding.component';
+import { NewComponentComponent } from './new-component/new-component.component';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    CommonModule,
+  ],
+  declarations: [AppComponent, DataBindingComponent, NewComponentComponent],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
